72dd135de89b224aec548f7b9f0343873677e116
Uheeking <wert1847@naver.com>
2023-01-31 15:36:35 +0900

    create a validation rule

--- lib/pages/todoAdd.dart
+++ lib/pages/todoAdd.dart
@@ -2,14 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:intl/intl.dart';
+import 'package:get/get.dart';
 
-class TodoAdd extends StatelessWidget {
+class TodoAdd extends StatefulWidget {
   final DateTime day;
   const TodoAdd({super.key, required this.day});
 
   @override
+  State<TodoAdd> createState() => _TodoAddState();
+}
+
+class _TodoAddState extends State<TodoAdd> {
+  final myController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  String? name = '';
+  String? description = '';
+
+  @override
   Widget build(BuildContext context) {
-    final date = DateFormat('yyyy년 MM월 dd일').format(day);
+    final date = DateFormat('yyyy년 MM월 dd일').format(widget.day);
 
     return GestureDetector(
       onTap: () {
@@ -21,6 +32,7 @@ class TodoAdd extends StatelessWidget {
           centerTitle: true,
         ),
         body: Form(
+          key: formKey,
           child: Container(
             padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
             child: SingleChildScrollView(
@@ -32,8 +44,26 @@ class TodoAdd extends StatelessWidget {
                     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
                   ),
                 ),
-                TextField(
+                TextFormField(
+                  onChanged: (value) {
+                    print('myController: $value');
+                  },
+                  controller: myController,
                   decoration: InputDecoration(hintText: '할 일을 적어주세요.'),
+                  onSaved: (val) {
+                    setState(() {
+                      this.name = val;
+                    });
+                  },
+                  validator: (val) {
+                    if (val!.isEmpty) {
+                      return '할 일은 필수사항입니다.';
+                    }
+                    if (val.length < 1) {
+                      return '할 일은 필수사항입니다.';
+                    }
+                    return null;
+                  },
                 ),
                 SizedBox(
                   height: 30,
@@ -46,13 +76,24 @@ class TodoAdd extends StatelessWidget {
                     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
                   ),
                 ),
-                TextField(
+                TextFormField(
                   maxLines: 5,
                   decoration: InputDecoration(
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(0)),
                       contentPadding: EdgeInsets.all(10.0),
                       hintText: '설명을 적어주세요.'),
+                  onSaved: (val) {
+                    setState(() {
+                      description = val;
+                    });
+                  },
+                  validator: (val) {
+                    if (val!.isEmpty) {
+                      return '설명은 필수사항입니다.';
+                    }
+                    return null;
+                  },
                 ),
                 SizedBox(
                   height: 30,
@@ -66,7 +107,7 @@ class TodoAdd extends StatelessWidget {
                   ),
                 ),
                 Align(
-                  alignment: Alignment(-1.0, 0),
+                  alignment: Alignment.centerLeft,
                   child: Container(
                     width: MediaQuery.of(context).size.width / 2.4,
                     height: 50,
@@ -89,7 +130,14 @@ class TodoAdd extends StatelessWidget {
                 ),
                 ElevatedButton(
                   onPressed: () {
-                    print('i am uheeking');
+                    if (formKey.currentState!.validate()) {
+                      formKey.currentState!.save();
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text(name! + 'uheee' + description!)));
+                      print(name);
+                      print(description);
+                      // validation 이 성공하면 true 가 리턴돼요!
+                    }
                   },
                   child: Container(
                     width: MediaQuery.of(context).size.width / 2.2,
